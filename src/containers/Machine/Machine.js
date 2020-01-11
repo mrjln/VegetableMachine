308e65a17a5ac7b5b893b27b4f9b1b8388dd09bf
Marjolein Tromp <marjolein.tromp@incentro.com>
2020-01-11 17:06:43 +0100

    start with spinning wheels

--- src/containers/Machine/Machine.js
+++ src/containers/Machine/Machine.js
@@ -1,17 +1,38 @@
-import React, { Component } from 'react'
-import './Machine.css'
-import MachineWindow from '../../components/MachineWindow/MachineWindow';
-import VegetableList from '../../assets/vegetables'
+import React, { Component } from "react";
+import "./Machine.css";
+import MachineWindow from "../../components/MachineWindow/MachineWindow";
+import VegetableList from "../../assets/vegetables";
 
 class Machine extends Component {
-    render() {
-        const vegetables = VegetableList
-        const listVegetables = vegetables.map(vegetable => {
-            return <li><MachineWindow vegetable={vegetable}/></li>
-        });
-        return  <div class="machine"><ul class="machine-windows">{listVegetables}</ul> </div>   
+    constructor(props) {
+        super(props);
+       // https://codepen.io/werter25/pen/MxRJJV
+      }
+
+
+  render() {
+    const vegetables = VegetableList;
+    const listVegetables = vegetables.map(vegetable => {
+      return (
+        <li>
+          <MachineWindow vegetable={vegetable} />
+        </li>
+      );
+    });
+
+    const spin = () => {
+        console.log("spinning")
     }
-    
+
+    return (
+      <div class="machine">
+        <ul class="machine-windows">{listVegetables}</ul>
+        <div>
+          <button class="go" onClick={spin}>Start spinning</button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Machine
+export default Machine;
