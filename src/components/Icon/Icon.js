f779c0c39c2ef11c3548d5c7fb907a51f219449d
Marjolein Tromp <marjolein.tromp@incentro.com>
2020-02-01 17:41:31 +0100

    Icon.js removed comment

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -3,10 +3,8 @@ import "./Icon.css";
 import VegetableIcons from "../../icons/index"
 
 function Icon(props) {
-    console.log(VegetableIcons[props.vegetableName]);
-   const vegetableicon = VegetableIcons[props.vegetableName];
-    return <div  className="vegetable-icon">{vegetableicon}</div>
-
+    const vegetableIcon = VegetableIcons[props.vegetableName];
+    return <div  className="vegetable-icon">{vegetableIcon}</div>
 }
 
 export default Icon
