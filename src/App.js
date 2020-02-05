ee3e06f3f11bd9ce3a4cee6ce92ff323747a6563
Marjolein Tromp <marjolein.tromp@incentro.com>
2020-02-05 15:50:25 +0100

    feat: add navigation to every page (wip)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Machine from './components/Machine/Machine'
 import Layout from './containers/Layout/Layout'
-import Navigation from "./containers/Navigation/Navigation";
 
 import {
     BrowserRouter as Router,
@@ -24,15 +23,14 @@ function App() {
     return (
         <Router>
             <div>
-                <Navigation navLinks={routerLinks}/>
                 <Switch>
                     <Route exact path="/">
-                        <Layout headerTitle={"Vegetable Machine"}>
+                        <Layout routerLinks={routerLinks} headerTitle={"Vegetable Machine"}>
                             <Machine/>
                         </Layout>
                     </Route>
                     <Route path="/about">
-                        <Layout headerTitle={"About thaaaat"}>
+                        <Layout routerLinks={routerLinks} headerTitle={"About thaaaat"}>
                             <h1> About bladiebla</h1>
                         </Layout>
                     </Route>
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Component} from "react";
-import Footer from "../Footer/Footer"
-import Header from "../Header/Header"
+import Footer from "./Footer/Footer"
+import Header from "./Header/Header"
+import Navigation from "./Navigation/Navigation";
+
 
 class Layout extends Component {
     render() {
         return <div>
             <Header headerTitle={this.props.headerTitle} />
+            <Navigation navLinks={this.props.routerLinks}/>
+
             {this.props.children}
             <Footer/>
         </div>
