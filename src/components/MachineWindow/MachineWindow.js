2f21a2a850b57a67dfbbfaaaf2b5028995b046ad
Marjolein Tromp <marjolein.tromp@incentro.com>
2020-04-11 18:54:06 +0200

    add blinking lights

--- src/components/MachineWindow/MachineWindow.js
+++ src/components/MachineWindow/MachineWindow.js
@@ -3,8 +3,20 @@ import "./MachineWindow.scss";
 
 function MachineWindow(props) {
 
+    const machineWidth = 300;
+    const amountOfLightsBottom = Math.round(machineWidth / 30);
+    const machineLigths = [];
+    for(let i = 0; i < amountOfLightsBottom; i++){
+
+        let style = {left: 25 + i * 30}
+        machineLigths.push(<div style={style} className="machinewindow__light-bulb"></div>)
+    }
+    console.log(machineLigths);
+
+
     return <React.Fragment>
         <div className="machinewindow">
+            {machineLigths}
             <div className="machinewindow__button">
                 <button className="button button--primary" onClick={props.spin}>Spin</button>
             </div>
