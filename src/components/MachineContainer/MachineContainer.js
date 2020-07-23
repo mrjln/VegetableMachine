0fc81a0e5cc73d3873d817145b5d1beb7c49a152
Marjolein Tromp <marjolein.tromp@incentro.com>
2020-07-23 11:41:48 +0200

    make polonaise function so to shuffle the array semi randomly

--- src/components/MachineContainer/MachineContainer.js
+++ src/components/MachineContainer/MachineContainer.js
@@ -6,15 +6,36 @@ import VegetableList from "../../assets/vegetables";
 
 class MachineContainer extends Component {
 
+
     getRandomListOfItems = (itemList) => {
         return (shuffleArray([...itemList]));
     };
 
+    getArrayAfterPolonaise = (initialList, amountOfSteps) => {
+        return initialList.map((item, index) => {
+            let selector = index + amountOfSteps; // 4,5,6,7,8,9,10,11
+            if (selector >= (initialList.length - 1)){ // 0,1,2,3
+                return initialList[selector - (initialList.length - 1)]
+            }
+            return initialList[selector]
+        })
+    };
+
+    getShuffledLists = () => {
+        const amountOfSlots = 12;
+        const amountOfReels = 3;
+        const initialList = this.getRandomListOfItems(VegetableList);
+        console.log(initialList);
+        const denominator = amountOfSlots / amountOfReels;
+        const list = [initialList];
+        const list2 = this.getArrayAfterPolonaise(initialList, denominator);
+        const list3 = this.getArrayAfterPolonaise(initialList, denominator + 4);
+        console.log(list2, list3);
+        return [initialList, list2, list3]
+    };
+
     render(){
-        const randomItemLists = [1,2,3].map(() => {
-            return this.getRandomListOfItems(VegetableList);
-        });
-        return <Machine shuffledItems={randomItemLists}/>
+        return <Machine shuffledItems={this.getShuffledLists()}/>
     }
 }
 
