880fffbef88b57443b4943c219a2f186799a9e66
Marjolein Tromp <marjolein.tromp@incentro.com>
2020-08-25 17:02:51 +0200

    change stacking order of rings and machine window

--- src/components/MachineWindow/MachineWindow.js
+++ src/components/MachineWindow/MachineWindow.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import "./MachineWindow.scss";
-import { svgPathProperties } from "svg-path-properties";
+import {svgPathProperties} from "svg-path-properties";
 
 
 function MachineWindow(props) {
 
     const getMachineLights = () => {
-        const amountOfLights = 24;
+        const amountOfLights = 21;
         const machineLights = [];
         const properties = new svgPathProperties("M298,101C297.325, 53.126, 233.796, 0, 149, 0C64.2043, 0, 0.742637, 53.126, 0, 101H298Z");
-        const length = properties.getTotalLength(); // 690.7935791015625
+        const length = properties.getTotalLength();
         for (let i = 0; i < amountOfLights; i++) {
-            const point = properties.getPointAtLength(i * 1/21 * length);
-            const circle = <circle key={i} className={"machine-window__light-bulb blinker-1"} cx={point.x} cy={point.y} r="5"> </circle>
+            const point = properties.getPointAtLength(i / amountOfLights * length);
+            const circle = <circle key={i}
+                                   className={`machine-window__light-bulb blinker-${i}`}
+                                   cx={point.x}
+                                   cy={point.y}
+                                   r="5">
+                                <animate
+                                    attributeType="XML"
+                                    attributeName="fill"
+                                    values="#f9de8a;#FFCE54;#f9de8a;#f9de8a"
+                                    dur="0.8s"
+                                    repeatCount="indefinite"
+                                />
+                             </circle>
             machineLights.push(circle)
         }
         return machineLights;
@@ -25,7 +37,6 @@ function MachineWindow(props) {
                  xmlns="http://www.w3.org/2000/svg">
                 <path id="curve" d="M298 101C297.325 53.126 233.796 0 149 0C64.2043 0 0.742637 53.126 0 101H298Z"
                       fill="#0f331f"/>
-
                 {getMachineLights()}
             </svg>
             <div className="machine-window__button">
