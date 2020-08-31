88327c2344483cce7504d5b6a559e3d4ded9c70d
Marjolein Tromp <marjolein.tromp@incentro.com>
2020-08-31 12:13:06 +0200

    fix bug of sticky button scrolling into view

--- src/components/MachineItemDetailPage/MachineItemDetailPage.tsx
+++ src/components/MachineItemDetailPage/MachineItemDetailPage.tsx
@@ -12,6 +12,14 @@ interface MachineItemDetailProps {
 
 class MachineItemDetailPage extends Component<MachineItemDetailProps> {
 
+
+    isScrolledIntoView(el: HTMLElement): boolean {
+        const rect = el.getBoundingClientRect();
+        const elemTop = rect && rect.top;
+        const elemBottom = rect && rect.bottom;
+        return elemTop && elemBottom ? elemTop >= 0 && elemBottom <= window.innerHeight : false
+    }
+
     componentDidMount() {
         document.addEventListener('scroll', this.trackScrolling);
     }
@@ -21,12 +29,14 @@ class MachineItemDetailPage extends Component<MachineItemDetailProps> {
     }
 
     trackScrolling = () => {
-        const wrappedElement = document.getElementById('footer');
         const cta = document.getElementById('cta');
-        if (wrappedElement && Math.round(wrappedElement.getBoundingClientRect().top) <= (window.innerHeight)) {
-            if (cta) cta.classList.add('machine-item-detail__on-the-spot-cta');
+        const sticky = document.getElementById('sticky-button');
+        if (cta)console.log(window.innerHeight - 200, Math.round(cta.getBoundingClientRect().top ));
+
+        if (cta && this.isScrolledIntoView(cta)) {
+            if (sticky) sticky.style.display = 'none';
         } else {
-            if (cta) cta.classList.remove('machine-item-detail__on-the-spot-cta');
+            if (sticky) sticky.style.display = 'flex';
         }
 
     };
@@ -66,7 +76,21 @@ class MachineItemDetailPage extends Component<MachineItemDetailProps> {
                 </ul>
             </div>
             <NutritionTable/>
-            <div className="machine-item-detail__sticky-cta" id='cta'>
+            <div className="machine-item-detail__cta machine-item-detail__cta--sticky" id='sticky-button'>
+                <a href={"https://www.ah.nl/allerhande/recepten-zoeken?Ntt=" + machineItem.name.nl}>
+                    <button className="button button--primary button--leave-site">
+                <span>
+                    Zoek een recept met {machineItem.name.nl}
+                </span>
+                        <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M19.7071 8.70711C20.0976 8.31658 20.0976 7.68342 19.7071 7.29289L13.3431 0.928932C12.9526 0.538408 12.3195 0.538408 11.9289 0.928932C11.5384 1.31946 11.5384 1.95262 11.9289 2.34315L17.5858 8L11.9289 13.6569C11.5384 14.0474 11.5384 14.6805 11.9289 15.0711C12.3195 15.4616 12.9526 15.4616 13.3431 15.0711L19.7071 8.70711ZM0 9H19V7H0V9Z"
+                                fill="#7C9073"/>
+                        </svg>
+                    </button>
+                </a>
+            </div>
+            <div className="machine-item-detail__cta machine-item-detail__cta--on-the-spot" id='cta'>
                 <a href={"https://www.ah.nl/allerhande/recepten-zoeken?Ntt=" + machineItem.name.nl}>
                     <button className="button button--primary button--leave-site">
                 <span>
