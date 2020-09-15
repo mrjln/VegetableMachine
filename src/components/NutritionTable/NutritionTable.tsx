8423d87dce66ee96f590829e00554dea8681174a
Marjolein Tromp <marjolein.tromp@incentro.com>
2020-09-15 15:33:10 +0200

    merge the RDI with the Item Nutrition Values

--- src/components/NutritionTable/NutritionTable.tsx
+++ src/components/NutritionTable/NutritionTable.tsx
@@ -4,156 +4,308 @@ import MachineItem from "../../utils/types/types"
 
 interface NutritionValue {
     name: string;
-    value: number;
+    rdi?: number;
+    value?: number;
     units?: string;
 }
+
 // Based on a male 19-30
 
 const NutritionValuesRDI: NutritionValue[] = [
     {
         name: 'kcal',
-        value: 2000,
+        rdi: 2000,
     },
     {
         name: 'kJ',
-        value: 10000,
+        rdi: 10000,
     },
     {
         name: 'water',
-        value: 100,
+        rdi: 100,
         units: 'gr/day'
     },
     {
         name: 'protein',
-        value: 100,
+        rdi: 100,
     },
     {
         name: 'carbonhydrates',
-        value: 100,
+        rdi: 100,
     },
     {
         name: 'sugars',
-        value: 100,
+        rdi: 100,
     },
     {
         name: 'fat',
-        value: 100,
+        rdi: 100,
     },
     {
         name: 'saturated',
-        value: 100,
+        rdi: 100,
     },
     {
         name: 'cholesterol',
-        value: 100,
+        rdi: 100,
     },
     {
         name: 'fibers',
-        value: 100,
+        rdi: 100,
     },
     {
         name: 'Vitamin A',
-        value: 800,
+        rdi: 800,
         units: 'µg/day '
     },
     {
         name: 'Vitamin B1',
-        value: 1.1,
+        rdi: 1.1,
         units: 'mg/day'
     },
     {
         name: 'Vitamin B2',
-        value: 	1.2,
+        rdi: 1.2,
         units: 'mg/day'
 
     },
     {
         name: 'Vitamin B3',
-        value: 16,
+        rdi: 16,
         units: 'mg/day'
 
     },
     // {
     //     name: 'Vitamin B4',
-    //     value: 100,
+    //     rdi: 100,
     // },
     {
         name: 'Vitamin B5',
-        value: 5,
+        rdi: 5,
         units: 'mg/day'
 
     },
     {
         name: 'Vitamin B6',
-        value: 1.7,
+        rdi: 1.7,
         units: 'mg/day'
     },
     // {
     //     name: 'Vitamin B11',
-    //     value: 100,
+    //     rdi: 100,
     // },
     {
         name: 'Vitamin B12',
-        value: 	2.4,
+        rdi: 2.4,
         units: 'µg/day '
     },
     {
         name: 'Vitamin C',
-        value: 60,
+        rdi: 60,
         units: 'mg/day'
     },
     {
         name: 'Vitamin D',
-        value: 20,
+        rdi: 20,
         units: 'µg/day '
     },
     {
         name: 'Vitamin E',
-        value: 15,
+        rdi: 15,
         units: 'mg/day'
     },
     {
         name: 'Vitamin K',
-        value: 120,
+        rdi: 120,
         units: 'µg/day '
     },
     {
         name: 'Natrium',
-        value: 100,
+        rdi: 100,
     },
     {
         name: 'Phosphorus',
-        value: 700,
+        rdi: 700,
         units: 'mg/day'
     },
     {
         name: 'Iron',
-        value: 18,
+        rdi: 18,
         units: 'mg/day'
     },
     {
         name: 'Magnesium',
-        value: 400,
+        rdi: 400,
         units: 'mg/day'
     },
     {
         name: 'Copper',
-        value: 900,
+        rdi: 900,
         units: 'µg/day '
     },
     {
         name: 'Zinc',
-        value: 11,
+        rdi: 11,
         units: 'mg/day'
     }
 ];
+
+const NutritionValuesDummy: NutritionValue[] = [
+    {
+        name: 'kcal',
+        value: 1111,
+    },
+    {
+        name: 'kJ',
+        value: 2222,
+    },
+    {
+        name: 'water',
+        value: 3,
+        units: 'gr/day'
+    },
+    {
+        name: 'protein',
+        value: 3,
+    },
+    {
+        name: 'carbonhydrates',
+        value: 3,
+    },
+    {
+        name: 'sugars',
+        value: 3,
+    },
+    {
+        name: 'fat',
+        value: 3,
+    },
+    {
+        name: 'saturated',
+        value: 3,
+    },
+    {
+        name: 'cholesterol',
+        value: 3,
+    },
+    {
+        name: 'fibers',
+        value: 3,
+    },
+    {
+        name: 'Vitamin A',
+        value: 3,
+        units: 'µg/day '
+    },
+    {
+        name: 'Vitamin B1',
+        value: 3,
+        units: 'mg/day'
+    },
+    {
+        name: 'Vitamin B2',
+        value: 3,
+        units: 'mg/day'
+
+    },
+    {
+        name: 'Vitamin B3',
+        value: 3,
+        units: 'mg/day'
+
+    },
+    // {
+    //     name: 'Vitamin B4',
+    //     rdi: 100,
+    // },
+    {
+        name: 'Vitamin B5',
+        value: 3,
+        units: 'mg/day'
+
+    },
+    {
+        name: 'Vitamin B6',
+        value: 3,
+        units: 'mg/day'
+    },
+    // {
+    //     name: 'Vitamin B11',
+    //     rdi: 100,
+    // },
+    {
+        name: 'Vitamin B12',
+        value: 3,
+        units: 'µg/day '
+    },
+    {
+        name: 'Vitamin C',
+        value: 3,
+        units: 'mg/day'
+    },
+    {
+        name: 'Vitamin D',
+        value: 3,
+        units: 'µg/day '
+    },
+    {
+        name: 'Vitamin E',
+        value: 3,
+        units: 'mg/day'
+    },
+    {
+        name: 'Vitamin K',
+        value: 3,
+        units: 'µg/day '
+    },
+    {
+        name: 'Natrium',
+        value: 3,
+    },
+    {
+        name: 'Phosphorus',
+        value: 333,
+        units: 'mg/day'
+    },
+    {
+        name: 'Iron',
+        value: 33,
+        units: 'mg/day'
+    },
+    {
+        name: 'Magnesium',
+        value: 33,
+        units: 'mg/day'
+    },
+    {
+        name: 'Copper',
+        value: 33,
+        units: 'µg/day '
+    },
+    // {
+    //     name: 'Zinc',
+    //     value: 33,
+    //     units: 'mg/day'
+    // }
+];
+
 
 interface NutritionTableProps {
     machineItem: MachineItem
 }
 
 
+
 function NutritionTable(props: NutritionTableProps) {
+   const mergeNutritionInfo = (completeNutriValues: NutritionValue[], itemValues:  NutritionValue[]) => {
+       return completeNutriValues.map(completeNutriValue => {
+           const itemValue = itemValues[completeNutriValues.findIndex(value => value.name === completeNutriValue.name)];
+           if(itemValue) completeNutriValue.value = itemValue.value
+           return completeNutriValue
+       });
+    };
+
+    const mergedNutritionInfo = mergeNutritionInfo(NutritionValuesRDI, NutritionValuesDummy);
     const item = props.machineItem;
     return <table>
         <thead>
@@ -164,17 +316,17 @@ function NutritionTable(props: NutritionTableProps) {
         </tr>
         </thead>
         <tbody>
-        {NutritionValuesRDI.map((value) => {
+        {mergedNutritionInfo.map((nutritionValue) => {
             return <React.Fragment>
                 <tr>
                     <td>
-                        {value.name}
+                        {nutritionValue.name}
                     </td>
                     <td>
-
+                        {nutritionValue.value ? nutritionValue.value : "---"}
                     </td>
                     <td>
-                        {value.value} <span>{value.units} </span>
+                        {nutritionValue.rdi} <span>{nutritionValue.units} </span>
                     </td>
 
                 </tr>
